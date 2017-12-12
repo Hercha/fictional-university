3aff2297b2b9bfb5c473090e86ea734b4c7070c2
fedjensa <jens.hedberg@medieinstitutet.se>
2017-12-12 09:24:54 +0100

    javascript search-overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,6 +1,29 @@
+import $ from 'jquery';
+
 class Search {
+    // 1. describe and create/initiate our object
+    // this.name = "Jane";
     constructor() {
-        alert("Hello Search");
+        this.openButton     = $(".js-search-trigger");
+        this.closeButton    = $('.search-overlay__close');
+        this.searchOverlay  = $(".search-overlay");
+        this.events();
+    }
+    // 2. events
+    // on this.head feels cold, wearsHat
+    events() {
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    // 3. methods (function, action....)
+    // wearsHat() {}
+    openOverlay() {
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass("search-overlay--active");
     }
 }
 
