b440a605a678f559931ad1c5b60839e93690a3f7
fedjensa <jens.hedberg@medieinstitutet.se>
2017-12-13 01:27:48 +0100

    javascript search functionality rest api

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -56,7 +56,7 @@ class Search {
             this.resultsDiv.html(`
                 <h2 class="search-overlay_section-title">General Information</h2>
                 ${combineResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-                   ${combineResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')} 
+                   ${combineResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == 'post' ? `by ${item.authorName}` : ''} </li>`).join('')} 
                 ${combineResults.length ? '</ul>' : ''}
             `);
             this.isSpinnerVisible = false;
